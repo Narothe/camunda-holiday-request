193cb433502fc81f5427b20e57db4d54fdc7fcef
Kamil Dereń <104890694+DwiN3@users.noreply.github.com>
2025-01-19 13:45:01 +0100

    Dodanie modeli bazy danych

---
+++ src/main/java/com/holiday/request/model/Employee.java
@@ -0,0 +1,32 @@
+package com.holiday.request.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LeaveRequest> leaveRequests;
+}
