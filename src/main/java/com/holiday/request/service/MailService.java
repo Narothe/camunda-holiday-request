f29ac78177b6a43d497712261641b2645842527a
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2025-01-19 14:34:51 +0100

    feat: send mail

---
+++ src/main/java/com/holiday/request/service/MailService.java
@@ -0,0 +1,32 @@
+package com.holiday.request.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender emailSender;
+    private final String SUBJECT = "Odmowa urlopu";
+    private final String BODY = "Witam, niestety nie możemy zatwierdzić Twojego urlopu w tym terminie.";
+
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public void sendEmail(String toEmail) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(sender);
+        message.setTo(toEmail);
+        message.setSubject(SUBJECT);
+        message.setText(BODY);
+
+        emailSender.send(message);
+        System.out.println("dziala");
+    }
+}
